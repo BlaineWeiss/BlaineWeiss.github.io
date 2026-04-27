a67f0b092e9d39fb4d62feb0dc737cee2b6bbeb9
Blaine Weiss <blaine.weiss@uky.edu>
2026-04-27 16:21:38 -0400

    Adjust trigger point for lava animation

--- assets/js/stone-lava-trigger-animation.js
+++ assets/js/stone-lava-trigger-animation.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
       Trigger when the top of the hero reaches about 25% down from
       the top of the viewport.
     */
-    const triggerPoint = window.innerHeight * 0.5;
+    const triggerPoint = window.innerHeight * 1.1;
 
     const shouldAnimate =
       rect.top <= triggerPoint &&
