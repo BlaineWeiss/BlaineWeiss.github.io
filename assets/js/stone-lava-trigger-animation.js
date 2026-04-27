e2823e68ef870c067afe538a975238d41c17aba0
Blaine Weiss <blaine.weiss@uky.edu>
2026-04-27 16:55:00 -0400

    Increase frame duration from 30 to 90

--- assets/js/stone-lava-trigger-animation.js
+++ assets/js/stone-lava-trigger-animation.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let intervalId = null;
   let hasStarted = false;
 
-  const frameDuration = 30;
+  const frameDuration = 90;
 
   function setActiveFrame(index) {
     frames.forEach((frame, i) => {
