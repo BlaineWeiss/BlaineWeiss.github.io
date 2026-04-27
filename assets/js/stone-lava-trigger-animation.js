ce7a23db40c6f747ee960a286c36c1d09b0ba5b4
Blaine Weiss <blaine.weiss@uky.edu>
2026-04-27 16:44:12 -0400

    Reduce frame duration from 333ms to 100ms

--- assets/js/stone-lava-trigger-animation.js
+++ assets/js/stone-lava-trigger-animation.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let intervalId = null;
   let hasStarted = false;
 
-  const frameDuration = 333;
+  const frameDuration = 100;
 
   function setActiveFrame(index) {
     frames.forEach((frame, i) => {
