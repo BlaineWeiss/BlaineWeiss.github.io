5e3563baddc5a4fc8dded0c1d54d798c1a824d05
Blaine Weiss <blaine.weiss@uky.edu>
2026-04-27 17:04:00 -0400

    Increase frame duration from 850ms to 2000ms

--- assets/js/stone-lava-trigger-animation.js
+++ assets/js/stone-lava-trigger-animation.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let intervalId = null;
   let hasStarted = false;
 
-  const frameDuration = 850;
+  const frameDuration = 2000;
 
   function setActiveFrame(index) {
     frames.forEach((frame, i) => {
